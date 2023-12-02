86c22c140a18bbb60588a97e9ecedaea86754064
John Gualteros <gualterosjohn40@gmail.com>
2023-12-02 15:00:01 -0500

    adding react for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-import vue from "@astrojs/vue";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  integrations: [react()]
 });
